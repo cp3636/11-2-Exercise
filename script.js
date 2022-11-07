6db0c47704ae9fa1814295cfcc4324fc1ed291ed
cp3636 <cp3636@nyu.edu>
2022-11-07 09:19:56 -0500

    help
    help

---
+++ script.js
@@ -0,0 +1,18 @@
+const = document.getElementId ("Submit")
+const btn = document.querySelector(".button");
+let load;
+
+btn.classList.add("click");
+ btn.classList.remove("click");
+
+
+function myFunction(){
+    load = setTimeout(alertFunction, 1000);
+}
+
+function alertFunction(){
+    if (button.value  == "click"){
+        button.value !== ''
+}
+
+}
